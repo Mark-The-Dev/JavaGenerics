3eac1017b71bb1fb5e71aa479a06797f15eec179
Mark <SwiftTactics@gmail.com>
2021-02-07 18:38:25 -0500

    Added generic type and extends player to team

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,20 @@ public class Main {
         BaseballPlayer pat = new BaseballPlayer("Pat");
         SoccerPlayer matt = new SoccerPlayer("Matt");
 
-        Team buffaloBills = new Team("Buffalo Bills");
+        Team<FootballPlayer> buffaloBills = new Team<>("Buffalo Bills");
         buffaloBills.addPlayer(joe);
-        buffaloBills.addPlayer(pat);
-        buffaloBills.addPlayer(matt);
+//        buffaloBills.addPlayer(pat);
+//        buffaloBills.addPlayer(matt);
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("chicago cubs");
+        baseballTeam.addPlayer(pat);
+
+        //Fixed by extending T in Team class to extend player.
+        Team<SoccerPlayer> fixedTeam = new Team<>("Soccer Team");
+        fixedTeam.addPlayer(matt);
+
+
+
         System.out.println(buffaloBills.numPlayers());
     }
 
